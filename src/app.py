7e404a62630aea7d4b9a7677a2219cf6b9701502
Aaron Campos <aaroncm611@gmail.com>
2025-01-17 10:36:29 +0100

    empezamos nuevo proyecto Flask

---
+++ src/app.py
@@ -0,0 +1,29 @@
+from flask import Flask
+from dotenv import load_dotenv
+
+load_dotenv()
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'Hola mundo'
+
+@app.route('/add_contact')
+def add_contact():
+    return 'ADD_CONTACT'
+
+@app.route('/edit_contact')
+def edit_contact():
+    return 'EDIT CONTACT'
+
+@app.route('/update_contact')
+def update_contact():
+    return 'UPDATE CONTACTO'
+
+@app.route('/delete_contact')
+def delete_contact():
+    return 'delete_contact'
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
